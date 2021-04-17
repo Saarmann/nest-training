9f1d7ea124f49c295f4e80ccdf6b2fbee8bcf35e
saarmann <kaspar.saarmann@gmail.com>
2021-04-17 22:59:01 +0300

    validation to task module

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,14 @@
 /* eslint-disable prettier/prettier */
+import { IsIn, IsNotEmpty, IsOptional } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTaskFilterDto {
+
+    @IsOptional()
+    @IsIn([TaskStatus.OPEN, TaskStatus.DONE, TaskStatus.IN_PROGRESS])
     status: TaskStatus;
+
+    @IsOptional()
+    @IsNotEmpty()
     search: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -27,8 +27,15 @@ export class TasksService {
     return tasks;
    };
 
+
    getTaskById(id: string): Task {   
-    return this.tasks.find(task => task.id === id);   
+    const found = this.tasks.find(task => task.id === id);   
+
+    if(!found) {
+      throw new NotFoundException(`Task with ID ${id} not found`);  
+    }
+
+    return found;
    }
 
    createTask(createTaskDto: CreateTaskDto): Task {
@@ -46,7 +53,10 @@ export class TasksService {
    }
 
    deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+
+    const found = this.getTaskById(id);
+    
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
    }
 
    updateTaskStatus(id: string, status: TaskStatus): Task {
